994af974a4d9dd2df5f033aade0176dfbe482b7e
Alex Carbajo <alexcarbajo@gmail.com>
2018-12-01 14:41:18 +0100

    Some modules

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Test',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,21 +10,14 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Texto helmet description' },
+        { name: 'keywords', content: 'keywords' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+
+    <Header />
+
+    {children()}
   </div>
 )
 
